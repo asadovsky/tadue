1ecbf504d40a767f6b639cee90114c0282c88a53
Adam Sadovsky <asadovsky@gmail.com>
2012-11-25 22:49:02 -0800

    Use css sprites for plus/minus to avoid janky load

--- misc/html/images.js
+++ misc/html/images.js
@@ -10,7 +10,7 @@ var makeImage = function (canvasSelector, imgSelector) {
 //////////////////////////////
 // Logo
 
-var drawOneLogo = function (spanSelector, canvasSelector, font) {
+var drawOneLogo = function (canvasSelector, spanSelector, font) {
   var canvas = $(canvasSelector).get(0);
   canvas.width = $(spanSelector).width();
   canvas.height = $(spanSelector).height();
@@ -29,11 +29,11 @@ var drawOneLogo = function (spanSelector, canvasSelector, font) {
 };
 
 var drawLogos = function () {
-  drawOneLogo('#logo', '#clogo', '60px Alice');
-  makeImage('#clogo', '#ilogo');
+  drawOneLogo('#logo', '#css_logo', '60px Alice');
+  makeImage('#logo', '#ilogo');
 
-  drawOneLogo('#logo_small', '#clogo_small', '40px Alice');
-  makeImage('#clogo_small', '#ilogo_small');
+  drawOneLogo('#logo_small', '#css_logo_small', '40px Alice');
+  makeImage('#logo_small', '#ilogo_small');
 };
 
 // Add a delay to allow font to load.
@@ -43,6 +43,7 @@ setTimeout(drawLogos, 200);
 // Icons
 
 var RADIUS = 8;
+var DIAMETER = 2 * RADIUS;
 var BORDER_WIDTH = 1;
 var LINE_LENGTH = 10;
 
@@ -53,49 +54,45 @@ var initCanvas = function (canvasSelector, width, height) {
   return canvas.getContext('2d');
 };
 
-var initCircleCanvas = function (canvasSelector) {
-  return initCanvas(canvasSelector, 2 * RADIUS, 2 * RADIUS);
-};
-
-var makeCircle = function (ctx, color, alpha) {
+var makeCircle = function (ctx, startX, color, alpha) {
   ctx.fillStyle = color;
   ctx.beginPath();
-  ctx.arc(RADIUS, RADIUS, RADIUS, 0, Math.PI * 2, true);
+  ctx.arc(startX + RADIUS, RADIUS, RADIUS, 0, Math.PI * 2, true);
   ctx.fill();
 
   var oldAlpha = ctx.globalAlpha;
   ctx.globalAlpha = alpha;
   ctx.fillStyle = '#fff';
   ctx.beginPath();
-  ctx.arc(RADIUS, RADIUS, RADIUS - BORDER_WIDTH, 0, Math.PI * 2, true);
+  ctx.arc(startX + RADIUS, RADIUS, RADIUS - BORDER_WIDTH, 0, Math.PI * 2, true);
   ctx.fill();
   ctx.globalAlpha = oldAlpha;
 };
 
-var makeLine = function (ctx, color, vertical) {
+var makeLine = function (ctx, startX, color, vertical) {
   ctx.strokeStyle = color;
   ctx.lineWidth = 2;
   ctx.beginPath();
   if (vertical) {
-    ctx.moveTo(RADIUS, RADIUS - LINE_LENGTH / 2);
-    ctx.lineTo(RADIUS, RADIUS + LINE_LENGTH / 2);
+    ctx.moveTo(startX + RADIUS, RADIUS - LINE_LENGTH / 2);
+    ctx.lineTo(startX + RADIUS, RADIUS + LINE_LENGTH / 2);
   } else {
-    ctx.moveTo(RADIUS - LINE_LENGTH / 2, RADIUS);
-    ctx.lineTo(RADIUS + LINE_LENGTH / 2, RADIUS);
+    ctx.moveTo(startX + RADIUS - LINE_LENGTH / 2, RADIUS);
+    ctx.lineTo(startX + RADIUS + LINE_LENGTH / 2, RADIUS);
   }
   ctx.stroke();
 };
 
-var makeMinus = function (ctx, color) {
-  makeLine(ctx, color, false);
+var makeMinus = function (ctx, startX, color) {
+  makeLine(ctx, startX, color, false);
 };
 
-var makePlus = function (ctx, color) {
-  makeLine(ctx, color, false);
-  makeLine(ctx, color, true);
+var makePlus = function (ctx, startX, color) {
+  makeLine(ctx, startX, color, false);
+  makeLine(ctx, startX, color, true);
 };
 
-var X_SIZE = 9;
+var X_WIDTH = 9;
 var X_LINE_WIDTH = 2;
 var X_COLOR = '#555';
 
@@ -104,46 +101,34 @@ var makeX = function (ctx) {
   ctx.lineWidth = X_LINE_WIDTH;
   ctx.beginPath();
   ctx.moveTo(0, 0);
-  ctx.lineTo(X_SIZE, X_SIZE);
-  ctx.moveTo(0, X_SIZE);
-  ctx.lineTo(X_SIZE, 0);
+  ctx.lineTo(X_WIDTH, X_WIDTH);
+  ctx.moveTo(0, X_WIDTH);
+  ctx.lineTo(X_WIDTH, 0);
   ctx.stroke();
 };
 
-var FONT_COLOR = '#66c';
+var PURPLE = '#66c';
 var LIGHT_GRAY = '#ddd';
 var BLACK = '#000';
 
 var drawIcons = function () {
-  var ctx = initCircleCanvas('#plus');
-  makePlus(ctx, FONT_COLOR);
+  var ctx = initCanvas('#plus', 3 * DIAMETER, DIAMETER);
+  makePlus(ctx, 0, PURPLE);
+  makeCircle(ctx, DIAMETER, LIGHT_GRAY, 0);
+  makePlus(ctx, DIAMETER, PURPLE);
+  makeCircle(ctx, 2 * DIAMETER, LIGHT_GRAY, 0);
+  makePlus(ctx, 2 * DIAMETER, BLACK);
   makeImage('#plus', '#iplus');
 
-  ctx = initCircleCanvas('#plus-hover');
-  makeCircle(ctx, LIGHT_GRAY, 0);
-  makePlus(ctx, FONT_COLOR);
-  makeImage('#plus-hover', '#iplus-hover');
-
-  ctx = initCircleCanvas('#plus-click');
-  makeCircle(ctx, LIGHT_GRAY, 0);
-  makePlus(ctx, BLACK);
-  makeImage('#plus-click', '#iplus-click');
-
-  ctx = initCircleCanvas('#minus');
-  makeMinus(ctx, FONT_COLOR);
+  ctx = initCanvas('#minus', 3 * DIAMETER, DIAMETER);
+  makeMinus(ctx, 0, PURPLE);
+  makeCircle(ctx, DIAMETER, LIGHT_GRAY, 0);
+  makeMinus(ctx, DIAMETER, PURPLE);
+  makeCircle(ctx, 2 * DIAMETER, LIGHT_GRAY, 0);
+  makeMinus(ctx, 2 * DIAMETER, BLACK);
   makeImage('#minus', '#iminus');
 
-  ctx = initCircleCanvas('#minus-hover');
-  makeCircle(ctx, LIGHT_GRAY, 0);
-  makeMinus(ctx, FONT_COLOR);
-  makeImage('#minus-hover', '#iminus-hover');
-
-  ctx = initCircleCanvas('#minus-click');
-  makeCircle(ctx, LIGHT_GRAY, 0);
-  makeMinus(ctx, BLACK);
-  makeImage('#minus-click', '#iminus-click');
-
-  ctx = initCanvas('#close', X_SIZE, X_SIZE);
+  ctx = initCanvas('#close', X_WIDTH, X_WIDTH);
   makeX(ctx);
   makeImage('#close', '#iclose');
 };
